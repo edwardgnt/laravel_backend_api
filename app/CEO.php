dce7cf2078f2c1db59455317fdc4b9ded2b6d5fb
vagrant <vagrant@homestead>
2020-08-06 05:35:48 +0000

    first commit

---
+++ app/CEO.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CEO extends Model
+{
+    protected $fillable = [
+        'name',
+        'company_name',
+        'year',
+        'company_headquarters',
+        'what_company_does'
+    ];
+}
